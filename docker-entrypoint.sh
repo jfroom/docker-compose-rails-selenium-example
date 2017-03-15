7cc910cc2afcab7ca7ec0d5609211b9a71879707
Jeffrey Froom <jeff@jfmk.co>
2017-03-14 20:42:30 -0700

    Add ruby verison from Gemfile, remove echos from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,8 @@
 set -e
 # Exit on fail
 
-echo "bundle path = $BUNDLE_PATH"
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
 
-echo 'entrypoint'
-# Finally call command issued to the docker service
 exec "$@"
-
+# Finally call command issued to the docker service
